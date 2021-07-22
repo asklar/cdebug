ee08c460a762d51b14da3f4bc54165d319b983ea
Alexander Sklar <asklar@microsoft.com>
2021-07-21 22:13:46 -0700

    Add tests, fix cdebug to work with non-const string types

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -3,21 +3,34 @@
 
 #include <iostream>
 #include <cdebug.h>
+#include <string_view>
+#include <winrt/base.h>
 
 int main()
 {
-  cdebug << "Test 123";
-  cdebug.flush();
-  std::cout << "Hello World!\n";
+  cdebug << "const char*";
+  cdebug << L"Wide char";
+  cdebug << 123;
+
+  std::string str{ "hello" };
+  cdebug << str;
+
+  std::wstring wstr{ L"hello" };
+  cdebug << wstr;
+
+  std::string_view sv{ "string_view" };
+  cdebug << sv;
+
+  std::wstring_view wsv{ L"wstring_view" };
+  cdebug << wsv;
+
+  char buf[] = { 'B', 'u', 'f', '\0' };
+  cdebug << buf;
+
+  char buf2[4] = { 'B', 'u', 'f', '\0' };
+  cdebug << buf2;
+
+  cdebug << winrt::hstring(L"hstring");
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
