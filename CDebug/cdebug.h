07376c96d9617755e5f6fb9974650a25b660f519
Alexander Sklar <asklar@microsoft.com>
2021-09-28 13:51:27 -0700

    Fix DEBUG_HRESULT_ERROR

--- CDebug/cdebug.h
+++ CDebug/cdebug.h
@@ -5,52 +5,57 @@
 #include <sstream>
 #include <string_view>
 
-struct cdebug_t : std::streambuf, std::ostream {
+struct cdebug_t : std::streambuf, std::ostream
+{
   cdebug_t() : std::ostream(this) {}
 
   std::streambuf::int_type overflow(std::streambuf::int_type c)
   {
-    char v[2] = { (char)c, 0 };
+    char v[2] = {(char)c, 0};
     OutputDebugStringA(v);
     return 0;
   }
 };
 
-inline cdebug_t& operator<<(cdebug_t& os, const wchar_t* v) {
+inline cdebug_t &operator<<(cdebug_t &os, const wchar_t *v)
+{
   OutputDebugStringW(v);
   return os;
 }
 
-inline cdebug_t& operator<<(cdebug_t& os, const char* v) {
+inline cdebug_t &operator<<(cdebug_t &os, const char *v)
+{
   OutputDebugStringA(v);
   return os;
 }
 
-inline cdebug_t& operator<<(cdebug_t& os, const std::string& v) {
+inline cdebug_t &operator<<(cdebug_t &os, const std::string &v)
+{
   OutputDebugStringA(v.c_str());
   return os;
 }
 
-inline cdebug_t& operator<<(cdebug_t& os, const std::wstring& v) {
+inline cdebug_t &operator<<(cdebug_t &os, const std::wstring &v)
+{
   OutputDebugStringW(v.c_str());
   return os;
 }
 
-inline cdebug_t& operator<<(cdebug_t& os, const std::string_view& v) {
+inline cdebug_t &operator<<(cdebug_t &os, const std::string_view &v)
+{
   OutputDebugStringA(v.data());
   return os;
 }
 
-inline cdebug_t& operator<<(cdebug_t& os, const std::wstring_view& v) {
+inline cdebug_t &operator<<(cdebug_t &os, const std::wstring_view &v)
+{
   OutputDebugStringW(v.data());
   return os;
 }
 
-
 __declspec(selectany) cdebug_t cdebug;
-
 
 #define DEBUG_HRESULT_ERROR(e)                    \
   cdebug << __FILE__ << " (" << __LINE__ << ") "; \
   cdebug.flush();                                 \
-  cwdebug << e.message().c_str() << std::endl;
+  cdebug << e.message().c_str() << std::endl;
